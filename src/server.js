0b112c4bb81b7a8bbc30f67c673bbb6350324a29
Eduardo <eduardoestudos12@gmail.com>
2024-06-02 20:42:39 -0300

    A

--- src/server.js
+++ src/server.js
@@ -9,8 +9,8 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/register", (request, response) => {
-  const absolutePath = path.join(__dirname, "public", "register.html");
-  response.sendFile(absolutePath);
+  const absolutePathRegister = path.join(__dirname, "public", "register.html");
+  response.sendFile(absolutePathRegister);
 });
 
 app.post("/register", (request, response) => {
@@ -22,6 +22,14 @@ app.post("/register", (request, response) => {
     }
     console.log(result);
   });
+
+  response.redirect("/test");
+});
+
+app.get("/test", (request, response) => {
+  const url = path.join(__dirname, "public", "teste.html");
+  console.log("Teste");
+  response.send("TESTE");
 });
 
 app.listen(port, () => {
